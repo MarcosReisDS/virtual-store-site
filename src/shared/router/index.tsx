0309dd30fd0d819344aa3577904a860d135ddbdc
marcos reis <marcos.reis349@gmail.com>
2024-02-09 17:10:13 -0300

    added route product

--- src/shared/router/index.tsx
+++ src/shared/router/index.tsx
@@ -4,6 +4,7 @@ import Home from "../../view/Home";
 import Store from "../../view/Store";
 import About from "../../view/About";
 import Service from "../../view/Service";
+import Product from "../components/Product";
 
 interface IRouter { }
 const Router: FC<IRouter> = () => {
@@ -11,6 +12,7 @@ const Router: FC<IRouter> = () => {
         <Routes>
             <Route path="/inicio" element={<Home />} />
             <Route path="/loja/masculino" element={<Store />} />
+            <Route path="/loja/masculino/1" element={<Product />} />
             <Route path="/loja/feminino" element={<Store />} />
             <Route path="/loja/promocoes" element={<Store />} />
             <Route path="/sobre" element={<About />} />
