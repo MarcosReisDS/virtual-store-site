c48d89a39ab1e3b7b25a505c8dddb6d4f81755d1
marcos reis <marcos.reis349@gmail.com>
2024-02-09 15:13:43 -0300

    navigation

--- src/shared/components/NavBar/index.tsx
+++ src/shared/components/NavBar/index.tsx
@@ -2,40 +2,73 @@ import { FC, useState } from "react"
 import "./styles.scss"
 import { BsFillHandbagFill } from "react-icons/bs";
 import { IoIosArrowDown } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 interface INavBar { }
 const NavBar: FC<INavBar> = () => {
 
+    const navigate = useNavigate()
     const [openMobile, setOpenMobile] = useState<boolean>(false)
     const [openModalMobile, setOpenModalMobile] = useState<boolean>(false)
 
+    const rotas = [
+        { name: "Início", url: "/inicio" },
+        {
+            name: "Loja", class: "loja", drop: [
+                { name: "Masculino", url: "/loja/masculino" },
+                { name: "Feminino", url: "/loja/feminino" },
+                { name: "Promoções", url: "/loja/promocoes" }
+            ]
+        },
+        { name: "Sobre AS", url: "/sobre" },
+        { name: "Atendimento ao Cliente", url: "/atendimento" },
+    ]
 
     return (
         <div className="container-nav-bar">
             <div className="container-logo">
-                <div className="logo">
+                <div className="logo" onClick={() => navigate(`${rotas[0].url}`)}>
                     <h1>as</h1>
                     <span />
                 </div>
-                <div className="name-logo">
+                <div className="name-logo" onClick={() => navigate(`${rotas[0].url}`)}>
                     <p>atual sneakers</p>
                 </div>
             </div>
             <div className="container-navigation">
                 <ul className="navigation pages">
-                    <li><a>Início</a></li>
-                    <li className="loja"><a>Loja</a>
-                        <ul>
-                            <li><a>Masculino</a></li>
-                            <li><a>Feminino</a></li>
-                            <li><a>Promoções</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Sobre AS</a></li>
-                    <li><a>Atendimento ao Cliente</a></li>
+                    {rotas.map((item) => (
+                        <>
+                            <li className={item.class}><a onClick={() => navigate(`${item.url}`)}>{item.name}</a>
+                                {item.name == "Loja" ?
+                                    <>
+                                        <ul>
+                                            {item.drop?.map((drop) => (
+                                                <>
+                                                    <li><a onClick={() => navigate(`${drop.url}`)}>{drop.name}</a></li>
+                                                </>
+                                            ))}
+                                        </ul>
+                                    </>
+
+                                    :
+                                    ""
+                                }
+                            </li>
+                            {/* <li className="loja"><a>Loja</a>
+                                <ul>
+                                    <li><a>Masculino</a></li>
+                                    <li><a>Feminino</a></li>
+                                    <li><a>Promoções</a></li>
+                                </ul>
+                            </li>
+                            <li><a>Sobre AS</a></li>
+                            <li><a>Atendimento ao Cliente</a></li> */}
+                        </>
+                    ))}
                 </ul>
                 <ul className="navigation user-cart">
-                    <li><a><img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjU5YTMwbGgybTh2cnRxa3FycnZsMTlxYXZtazVidG9nYTRnMmcxciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DFexVkRG7gX9oCy68r/giphy.gif" alt="Minha foto" /></a>
+                    <li><a><img src="https://i.ibb.co/VDCYXDG/user-5.png" alt="Minha foto" /></a>
                         <ul>
                             <li><a>Login</a></li>
                         </ul>
@@ -68,7 +101,7 @@ const NavBar: FC<INavBar> = () => {
                 </div>
                 <div className={openModalMobile ? "navigation" : "navigation close"}>
                     <div>
-                        <img src="https://i.pinimg.com/564x/d8/82/58/d882585e5fde45a1b1ee0886ba433021.jpg" />
+                        <img src="https://i.ibb.co/MNsF2kM/user.png" />
                         <span>Login</span>
                     </div>
                     <ul>
