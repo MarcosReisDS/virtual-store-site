bb9e6fba800c212f47ef85e2a204558735ace20e
marcos reis <marcos.reis349@gmail.com>
2024-02-09 19:15:08 -0300

    scroll between routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { FC } from "react"
+import { FC, useEffect } from "react"
 import Router from "./shared/router"
 import NavBar from "./shared/components/NavBar"
+import { useLocation } from "react-router-dom"
 
 interface IApp { }
 const App: FC<IApp> = () => {
+
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <>
       <NavBar />
