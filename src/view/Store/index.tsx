51c861763b80218827403f7193f8fcbdb41b1f74
marcos reis <marcos.reis349@gmail.com>
2024-02-09 10:15:06 -0300

    remove footer added component footer

--- src/view/Store/index.tsx
+++ src/view/Store/index.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from "react"
 import { SlArrowDown } from "react-icons/sl";
+import Footer from "../../shared/components/Footer";
 import "./styles.scss"
-import { FaFacebookF, FaInstagram, FaXTwitter } from "react-icons/fa6";
 
 interface IStore { }
 const Store: FC<IStore> = () => {
@@ -112,69 +112,10 @@ const Store: FC<IStore> = () => {
                                 <button>Adicionar ao carrinho</button>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
-            <div className="footer">
-                <div className="inputs">
-                    <div className="address">
-                        <div className="logo">
-                            <p>ENDEREÇO</p>
-                            <span />
-                        </div>
-                        <div className="body">
-                            <p>Rua Prates, 194 - Bom Retiro</p>
-                            <p>São Paulo - SP, 01121-000</p>
-                            <p>info@meusite.com</p>
-                            <p>Tel.: (11) 3456-7890</p>
-                        </div>
-                        <div className="social-media">
-                            <FaFacebookF className="media" />
-                            <FaXTwitter className="media" />
-                            <FaInstagram className="media" />
-                        </div>
-                    </div>
-                    <div className="contact-us">
-                        <div className="logo">
-                            <p>FALE CONOSCO</p>
-                            <span />
-                        </div>
-                        <div className="input">
-                            <div>
-                                <input type="text" placeholder="Nome" />
-                                <input type="text" placeholder="Email" />
-                                <input type="text" placeholder="Telefone" />
-                            </div>
-                            <div>
-                                <textarea placeholder="Insira sua mensagem aqui"></textarea>
-                            </div>
-                        </div>
-                        <div className="button">
-                            <button>Enviar</button>
-                        </div>
-
-                    </div>
-                    <div className="news">
-                        <div className="logo">
-                            <p>NOVIDADES</p>
-                            <span />
-                        </div>
-                        <div className="content">
-                            <p>Junte-se à lista de emails</p>
-                            <input type="text" placeholder="Insira seu email aqui" />
-                            <button>Assine agora</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="policy">
-                    <p>Política de Privacidade</p>
-                    <p>Política de Cookies</p>
-                </div>
-                <div className="baseboard">
-                    <p>© 2035 por ATUAL SNEAKERS. Orgulhosamente criado com Wix.com</p>
-                </div>
-            </div>
+            <Footer />
         </div>
     )
 }
