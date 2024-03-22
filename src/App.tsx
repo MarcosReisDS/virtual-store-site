ef190f62d4e8ba199f6a2ae5de80c80060c49bb4
marcos reis <marcos.reis349@gmail.com>
2024-03-22 18:59:08 -0300

    added lib 'jwt-decode' to know about jwt and adjust token placement

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import Router from "./shared/router"
 import NavBar from "./shared/components/NavBar"
 import { useLocation } from "react-router-dom"
 import ModalCart from "./shared/components/ModalCart"
-import Contexts, { IUserType, ICartType, IProductType } from './shared/contexts';
+import Contexts, { IUserType, ICartType, IProductType, Jwt } from './shared/contexts';
 import userApi from "./shared/services/user"
 import { getCookie } from "./shared/utils/cookies"
 import productApi from "./shared/services/product"
+import { jwtDecode } from "jwt-decode";
 
 interface IApp {
 
@@ -15,6 +16,7 @@ interface IApp {
 const App: FC<IApp> = () => {
   const { pathname } = useLocation()
   const [navbarValue, setNavbarValue] = useState<boolean>(false);
+  const [token, setToken] = useState<string>("")
 
   const [user, setUser] = useState<IUserType>({
     profile: '',
@@ -23,8 +25,9 @@ const App: FC<IApp> = () => {
     mail: '',
     password: '',
   })
-  const [cart, setCart] = useState<ICartType[]>([])
+  const [users, setUsers] = useState<IUserType[]>([])
 
+  const [cart, setCart] = useState<ICartType[]>([])
 
   const [products, setProducts] = useState<IProductType[]>([])
   const [product, setProduct] = useState<IProductType>({
@@ -39,16 +42,22 @@ const App: FC<IApp> = () => {
   useEffect(() => {
     window.scrollTo(0, 0)
 
-    userApi.listUser(getCookie("user").id).then((data: any) => {
-      setUser({
-        id: data?.id,
-        profile: data?.profile,
-        name: data?.name,
-        surname: data?.surname,
-        mail: data?.mail,
-        password: data?.password,
+    setToken(getCookie("token").token);
+
+    if (token) {
+      const jwt: Jwt = jwtDecode(token)
+
+      userApi.listUser(jwt?.email, token).then((data: any) => {
+        setUser({
+          id: data?.id,
+          profile: data?.profile,
+          name: data?.name,
+          surname: data?.surname,
+          mail: data?.mail,
+          password: data?.password,
+        })
       })
-    })
+    }
 
     productApi.listProducts().then((data: any) => {
       setProducts(data)
@@ -57,12 +66,16 @@ const App: FC<IApp> = () => {
     userApi.listCart().then((data: any) => {
       setCart(data)
     })
-  }, [pathname])
+
+  }, [pathname, token])
 
   return (
     <Contexts.Provider value={{
+      token,
       user,
+      users,
       setUser,
+      setUsers,
       cart,
       setCart,
       product,
